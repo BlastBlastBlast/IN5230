ba2582af57a2180a01ccc228b34cd91d80ef15ca
blastblastblast <lw.tynes@gmail.com>
2025-09-04 19:37:43 +0200

    step 2 complete, add add and delete

--- Deliverables/Oblig_1/Part_2.2/script.js
+++ Deliverables/Oblig_1/Part_2.2/script.js
@@ -3,17 +3,26 @@ function addCurrency(currency) {
     const ul = document.getElementById('currencyList');
     const li = document.createElement('li');
     li.textContent = currency;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+    deleteBtn.className = 'deleteButton';
+    deleteBtn.onclick = function() {
+        ul.removeChild(li);
+    }
+    
+    li.appendChild(deleteBtn);
     ul.appendChild(li);
 }
 
 // Event listeners
 // Add button appends to list
 document.getElementById('addButton').addEventListener('click', function() {
-    const input = document.getElementById('currencyInput').value.trim();
-    const currency = input;
+    const input = document.getElementById('currencyInput');
+    const currency = input.value.trim();
     if (currency) {
         addCurrency(currency);
-        currencyInput.value = '';
+        input.value = '';
     }
 });
 
@@ -23,7 +32,7 @@ document.getElementById('currencyInput').addEventListener('keydown', function(ev
         const currency = this.value.trim();
         if (currency) {
             addCurrency(currency);
-            this.value = ''; // Clear the text-box
+            this.value = '';
         }
     }
 });
